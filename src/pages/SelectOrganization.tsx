8ff17ec7fba63884907516f312e6aa81d4791381
Nic Laflamme <nic@laflamme.io>
2022-08-27 14:42:39 -0600

    yolo

--- src/pages/SelectOrganization.tsx
+++ src/pages/SelectOrganization.tsx
@@ -2,19 +2,34 @@ import React, { useEffect, useState } from "react";
 
 import { useAxios } from "../hooks/useAxios";
 
+type Organization = {
+  id: string;
+  domain: string;
+};
+
 export const SelectOrganization: React.FC = () => {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [organizations, setOrganizations] = useState<Organization[]>([]);
 
   const axios = useAxios();
 
   const handleMount = () => {
     const handleAsync = async () => {
       const result = await axios.get("/organizations");
-      console.log(result.data);
+      const data = result.data as Organization[];
+
+      setOrganizations(data);
       setIsLoaded(true);
     };
 
     handleAsync();
+  };
+
+  // TODO (Nic)
+  // - Navigate to a /org/:orgId route
+
+  const handleOrgClick = (organizationId: string) => {
+    console.log({ organizationId });
   };
 
   useEffect(handleMount, []);
@@ -23,5 +38,19 @@ export const SelectOrganization: React.FC = () => {
     return <div>I am fetching data...</div>;
   }
 
-  return <div>I am SelectOrganization</div>;
+  return (
+    <div>
+      {organizations.map((organization) => {
+        const handleClick = () => {
+          handleOrgClick(organization.id);
+        };
+
+        return (
+          <div key={organization.id} onClick={handleClick}>
+            {organization.domain}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
