3b10bc9f756f28f7af776da8792fcaeaf8cc7491
Nic Laflamme <nic@laflamme.io>
2022-08-27 13:15:29 -0600

    add controlled login fields

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+
+type Event = {
+  target: {
+    value: string;
+  };
+};
 
 export const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (event: Event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event: Event) => {
+    setPassword(event.target.value);
+  };
+
   return (
     <div className="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -31,6 +48,8 @@ export const Login: React.FC = () => {
                   type="email"
                   autoComplete="email"
                   required
+                  value={email}
+                  onChange={handleEmailChange}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
@@ -50,6 +69,8 @@ export const Login: React.FC = () => {
                   type="password"
                   autoComplete="current-password"
                   required
+                  onChange={handlePasswordChange}
+                  value={password}
                   className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                 />
               </div>
