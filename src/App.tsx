57e9113cff6722447af7ac5bcba70031800119a7
Nic Laflamme <nic@laflamme.io>
2022-08-27 15:38:51 -0400

    Add authenticated page (#3)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { Login } from "./pages/Login";
+import { Authenticated } from "./pages/Authenticated";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Authenticated />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Authenticated.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Authenticated: React.FC = () => {
+  return <div>I am authenticated</div>;
+};
